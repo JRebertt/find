4fd68df46c223363f486b9c02ee9d33184f4353e
jrbertt <joaorebertt0219@gmail.com>
2024-12-10 07:34:50 -0300

    chore: add cors oring

---
+++ src/https/app.ts
@@ -0,0 +1,37 @@
+import fastifyCors from '@fastify/cors'
+import fastify from 'fastify'
+import {
+  type ZodTypeProvider,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { errorHandler } from './error-handler'
+import jwtPlugin from './plugins/jwt'
+import rawBodyPlugin from './plugins/raw-body'
+import swaggerPlugin from './plugins/swagger'
+
+import healthRoutes from './routes/health'
+import { queryCpf } from './routes/queries/cpf'
+import { generateToken } from './routes/auth/generate-token'
+
+export const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+// Configurações adicionais
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+app.setErrorHandler(errorHandler)
+
+// Plugins
+app.register(fastifyCors, {
+  origin: true,
+  credentials: true,
+})
+app.register(rawBodyPlugin)
+app.register(swaggerPlugin)
+app.register(jwtPlugin)
+
+// Rotas
+app.register(healthRoutes, { prefix: '/health' })
+app.register(generateToken)
+app.register(queryCpf)
+// app.register(authRoutes, { prefix: '/auth' })
--- src/https/server.ts
+++ src/https/server.ts
@@ -1,37 +1,8 @@
-import fastifyCors from '@fastify/cors'
-import fastify from 'fastify'
-import {
-  type ZodTypeProvider,
-  serializerCompiler,
-  validatorCompiler,
-} from 'fastify-type-provider-zod'
-import { errorHandler } from './error-handler'
-import jwtPlugin from './plugins/jwt'
-import rawBodyPlugin from './plugins/raw-body'
-import swaggerPlugin from './plugins/swagger'
+import { app } from './app'
 
-import healthRoutes from './routes/health'
-import { queryCpf } from './routes/queries/cpf'
-import { generateToken } from './routes/auth/generate-token'
-
-const app = fastify().withTypeProvider<ZodTypeProvider>()
-
-// Configurações adicionais
-app.setSerializerCompiler(serializerCompiler)
-app.setValidatorCompiler(validatorCompiler)
-app.setErrorHandler(errorHandler)
-
-// Plugins
-app.register(fastifyCors)
-app.register(rawBodyPlugin)
-app.register(swaggerPlugin)
-app.register(jwtPlugin)
-
-// Rotas
-app.register(healthRoutes, { prefix: '/health' })
-app.register(generateToken)
-app.register(queryCpf)
-// app.register(authRoutes, { prefix: '/auth' })
+app.listen({ port: 3000, host: '0.0.0.0' }).then(() => {
+  console.log('HTTP server running 🚀')
+})
 
 //  app.listen({ port: 3000, host: '0.0.0.0' }).then(function (): void {
 //   console.log('HTTP server running 🚀');
@@ -39,7 +10,3 @@ app.register(queryCpf)
 //   app.log.error(err);
 //   process.exit(1);
 // });
-
-app.listen({ port: 3000, host: '0.0.0.0' }).then(() => {
-  console.log('HTTP server running 🚀')
-})
